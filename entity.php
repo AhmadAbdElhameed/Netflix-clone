1501cbb76ba0b02d56ee1ff6574e0df3722116b1
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-02-15 11:34:14 +0200

    adding all seasons with episodes

--- entity.php
+++ entity.php
@@ -1,4 +1,4 @@
-<?php
+<!-- 
     require_once('includes/header.php');
 
 
@@ -12,10 +12,27 @@
     $preview = new PreviewProvider($con,$userLoggedIn);
     echo $preview->createPreviewVideo($entity);
 
+    $seasonProvider = new SeasonProvider($con , $userLoggedIn);
+
+    echo $seasonProvider->create($entity); -->
 
 
 
 
 
 
+<?php
+require_once("includes/header.php");
+
+if(!isset($_GET["id"])) {
+    ErrorMessage::show("No ID passed into page");
+}
+$entityId = $_GET["id"];
+$entity = new Entity($con, $entityId);
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+echo $preview->createPreviewVideo($entity);
+
+$seasonProvider = new SeasonProvider($con, $userLoggedIn);
+echo $seasonProvider->create($entity);
 ?>
---
+++ includes/classes/SeasonProvider.php
@@ -0,0 +1,68 @@
+<?php
+    class SeasonProvider{
+        private $con , $username;
+
+
+        public function __construct($con,$username){
+
+            $this->con = $con;
+            $this->username = $username;
+        }
+
+        public function create($entity){
+
+            $seasons = $entity->getSeasons();
+
+            if(sizeof($seasons) == 0){
+                return;
+            }
+
+            $seasonsHTML ="";
+            foreach($seasons as $season){
+                $seasonNumber = $season->getSeasonNumber() . "<br>";
+
+                $videosHtml = "";
+
+                foreach($season->getVideos() as $video){
+                    $videosHtml .= $this->createVideoSquare($video);
+                }
+                $seasonsHTML .= "<div class='season'>
+                                    <h3>Season $seasonNumber</h3>
+                                    <div class='videos scrollbars_none'>
+                                        $videosHtml
+                                    </div>
+
+                                </div>";
+            }
+
+            return $seasonsHTML;
+
+        }
+
+        private function createVideoSquare($video){
+            $id = $video->getId();
+            $title = $video->getTitle();
+            $thumbnail = $video->getThumbnail();
+            $description = $video->getDescription();
+            $episodeNumber = $video->getEpisodeNumber();
+
+            return "<a href='watch.php?id=$id'>
+                        <div class ='episodeContainer'>
+                            <div class='contents'>
+                                <img src='$thumbnail'>
+                                <div class='videoInfo'>
+                                    <h4>$episodeNumber. $title</h4>
+                                    <span>$description</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>";
+        }
+
+
+
+    }
+
+
+
+?>
