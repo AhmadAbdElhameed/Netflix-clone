d9f203523ec24f46a6586d25c4a2d2de680d11ae
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-02-21 03:34:18 +0200

    adding continue watching with play video if it in progress

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -21,6 +21,8 @@
 
             $videoId = VideoProvider::getEntityVideoForUser($this->con , $id,$this->username);
             $video = new Video($this->con , $videoId);
+            $inProgress = $video->isInProgress($this->username);
+            $playButtonText = $inProgress ? "Continue watching" : "Play";
 
             $seasonEpisode = $video->getSeasonAndEpisode();
             $subHeading = $video->isMovie() ? "" : "<h4>$seasonEpisode</h4>";
@@ -35,7 +37,7 @@
                                 <h3>$name</h3>
                                 $subHeading
                                 <div>
-                                    <button onclick='watchVideo($videoId)'><i class='fa-solid fa-play'></i> Play</button>
+                                    <button onclick='watchVideo($videoId)'><i class='fa-solid fa-play'></i> $playButtonText</button>
                                     <button onclick='volumeToggle(this)'><i class='fa-solid fa-volume-xmark'></i></button>
                                 </div>
                             </div>
