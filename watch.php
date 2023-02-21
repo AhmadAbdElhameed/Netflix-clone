9469270fe834f9eba32c1507a3610c34308abfb5
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-02-21 02:08:35 +0200

    play next and replay current video has addedd

--- watch.php
+++ watch.php
@@ -17,23 +17,23 @@
         <h1><?php echo $video->getTitle(); ?></h1>
     </div>
 
-    <div class="videoControls upNext">
-        <button><i class="fa-solid fa-redo"></i></button>
+    <div class="videoControls upNext" style="display:none;">
+        <button onclick="restartVideo()"><i class="fa-solid fa-redo"></i></button>
 
         <div class="upNextContainer">
             <h2>Up Next:</h2>
             <h3><?php echo $upNextVideo->getTitle(); ?></h3>
             <h3><?php echo $upNextVideo->getSeasonAndEpisode(); ?></h3>
             
-            <button class="playNext">
-                <i class="fa-solid fa-play"></i>Play
+            <button class="playNext" onclick="watchVideo(<?php echo $upNextVideo->getId(); ?>)">
+                <i class="fa-solid fa-play"></i> Play
             </button>
 
 
         </div>
     </div>
 
-    <video controls autoplay>
+    <video controls autoplay onended="showUpNext()">
         <source src="<?php echo $video->getFilePath(); ?>" type="video/mp4">
     </video>
 </div>
